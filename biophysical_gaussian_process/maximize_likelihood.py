b42a5a9f44d77be6bcbf6c04bdbe5ae79e3bb2ba
fioriathos <40672529+fioriathos@users.noreply.github.com>
2020-10-21 09:58:12 +0200

    version to show minigroup

---
+++ biophysical_gaussian_process/maximize_likelihood.py
@@ -0,0 +1,82 @@
+import mean_and_cov_matrix as mc
+import scipy
+import numpy as np
+class maximize_likelihood(object):
+    def __init__(self,free,bounds,fixed={}):
+        """These 3 dictionary fixed the free param, constrained param, boundary"""
+        assert type(fixed)==dict
+        assert type(free)==dict;assert free!={}
+        self.fixed = fixed; self.free = free
+        self.bounds = bounds
+        assert set(fixed.keys())|set((free.keys()))==\
+	set(('ml','gl','sl2','mq','gq','sq2','b','sx2','sg2','sdx2','sdg2'))
+        # Fix them as model parameters
+        def set_att(key,val):
+            if key=='ml':self.ml=val
+            if key=='gl':self.gl=val
+            if key=='sl2':self.sl2=val
+            if key=='mq':self.mq=val
+            if key=='gq':self.gq=val
+            if key=='sq2':self.sq2=val
+            if key=='b':self.b=val
+            if key=='sx2':self.sx2=val
+            if key=='sg2':self.sg2=val
+            if key=='sdx2':self.sdx2=val
+            if key=='sdg2':self.sdg2=val
+        #set attributes
+        for key,val in free.items():
+            set_att(key,val)
+        for key,val in fixed.items():
+            set_att(key,val)
+    def fix_par(self,vec, **kwargs):
+        """From np.array vec divide in array the non fixed and dict the fix by giving fixed"""
+        from collections import OrderedDict
+        vecout = {}
+        tmp = OrderedDict([('ml',vec[0]),('gl',vec[1]),('sl2',vec[2]),\
+	      ('mq',vec[3]),('gq',vec[4]),('sq2',vec[5]),('b',vec[6])\
+	      ('sx2',vec[7]),('sg2',vec[8]),('sdx2',vec[9]),('sdg2',vec[10])])
+        for key in kwargs:
+            vecout[key]=tmp[key]
+            del tmp[key]
+        return np.array([tmp[key] for key in tmp]), vecout
+    def rebuild_param(self,vec,**kwargs):
+        """ Inverse operation than fix_par"""
+        from collections import OrderedDict
+        tmp = OrderedDict([('ml',None),('gl',None),('sl2',None),\
+                           ('mq',None),('gq',None),('sq2',None),('b',None),\
+                           ('sx2',None),('sg2',None),('sdx2',None),('sdg2',None)])
+        for key,val in kwargs.items():
+            assert val!=None, "Can't have None as fixed values"
+            tmp[key]=val
+        for key,val in tmp.items():
+            if val==None:
+                tmp[key]=vec[0]
+                vec = np.delete(vec,0)
+        return np.array([tmp[key] for key in tmp])
+    def initialize(self):
+        """Return the x np array"""
+        x0 = [None]*11
+        for i in self.free:
+            if i=='ml':x0[0]=self.free[i]
+            if i=='gl':x0[1]=self.free[i]
+            if i=='sl2':x0[2]=self.free[i]
+            if i=='mq':x0[3]=self.free[i]
+            if i=='gq':x0[4]=self.free[i]
+            if i=='sq2':x0[5]=self.free[i]
+            if i=='b':x0[6]=self.free[i]
+            if i=='sx2':x0[7]=self.free[i]
+            if i=='sg2':x0[8]=self.free[i]
+            if i=='sdx2':x0[9]=self.free[i]
+            if i=='sdg2':x0[10]=self.free[i]
+        x0 = [x for x in x0 if x is not None]
+        return np.array(x0)
+    def objective(self,x0,Y,m,C):
+        """Minus log lik"""
+        print(x0)
+        x = self.rebuild_param(x0,**self.fixed)
+        return -mc.posterior_likelihood(Y,m,C,*x,likelihood=True)
+    def maximize(self,Y,m,C):
+        """x0 start point, m&C mean and cov at time 0, Y[n,4] data"""
+        x0 = self.initialize()
+        return scipy.optimize.minimize(self.objective, x0, args=(Y,m,C),\
+                                       method='Powell', bounds=self.bounds)
---
+++ biophysical_gaussian_process/prior_distribution.py
@@ -0,0 +1,135 @@
+import numpy as np
+from numpy import sqrt as Sqrt
+from datetime import datetime
+from numpy import pi as Pi
+from numpy import exp as Exp
+from scipy.integrate import dblquad,quad
+######################################################################################
+####### GIVEN P(z0) find P(zt) i.e. nm and nC using OU model.      ###################
+####### Find also derivatives nm and nC w.r.t. hyperparameters     ###################
+######################################################################################
+def mean_x(t,bx,bg,bl,bq,Cxx,Cxg,Cxl,Cxq,Cgg,Cgl,Cgq,Cll,Clq,Cqq,ml,gl,sl2,mq,gq,sq2,b):
+    return bx+ml*t+(bl-ml)*(1-np.exp(-gl*t))/gl
+def mean_g(t,bx,bg,bl,bq,Cxx,Cxg,Cxl,Cxq,Cgg,Cgl,Cgq,Cll,Clq,Cqq,ml,gl,sl2,mq,gq,sq2,b):
+    return bg*Exp(-b*t) +quad(lambda tau:(Exp(bx + Cxx/2. + (b + bl + Cxl - gq)*tau + (Cll*tau**2)/2.)*(bq + Cxq - mq + Exp(gq*tau)*mq + Clq*tau))/Exp(b*t),0,t)[0]
+def mean_l(t,bx,bg,bl,bq,Cxx,Cxg,Cxl,Cxq,Cgg,Cgl,Cgq,Cll,Clq,Cqq,ml,gl,sl2,mq,gq,sq2,b):
+    return ml+(bl-ml)*np.exp(-gl*t)
+def mean_q(t,bx,bg,bl,bq,Cxx,Cxg,Cxl,Cxq,Cgg,Cgl,Cgq,Cll,Clq,Cqq,ml,gl,sl2,mq,gq,sq2,b):
+    return mq+(bq-mq)*np.exp(-gq*t)
+def cov_xx(t,bx,bg,bl,bq,Cxx,Cxg,Cxl,Cxq,Cgg,Cgl,Cgq,Cll,Clq,Cqq,ml,gl,sl2,mq,gq,sq2,b):
+    vx = sl2/(2*gl**3)*(2*gl*t-3+4*Exp(-gl*t)-Exp(-gl*t)**2)
+    return Cll*(1-Exp(-gl*t))**2/gl**2+2*Cxl*(1-Exp(-gl*t))/gl+Cxx+vx
+def cov_xg(t,bx,bg,bl,bq,Cxx,Cxg,Cxl,Cxq,Cgg,Cgl,Cgq,Cll,Clq,Cqq,ml,gl,sl2,mq,gq,sq2,b,nm):
+    return ((bg*bl + Cgl)/Exp(b*t) - (bg*bl + Cgl)/Exp((b + gl)*t) + ((bg*bx + Cxg)*gl)/Exp(b*t) - (bg*ml)/Exp(b*t) + (bg*ml)/Exp((b + gl)*t) + \
+           (bg*gl*ml*t)/Exp(b*t) + quad(lambda tau: Exp(bx + Cxx/2. - b*t - gl*t + (b + bl + Cxl - gq)*tau + (Cll*tau**2)/2.)*\
+           (-(bq*Cxl) - Cxl*Cxq + bq*Cxl*Exp(gl*t) + Cxl*Cxq*Exp(gl*t) + bq*bx*Exp(gl*t)*gl + Cxq*Exp(gl*t)*gl + bx*Cxq*Exp(gl*t)*gl + \
+            bq*Cxx*Exp(gl*t)*gl + Cxq*Cxx*Exp(gl*t)*gl + bq*ml + Cxq*ml - bq*Exp(gl*t)*ml - Cxq*Exp(gl*t)*ml + Cxl*mq - Cxl*Exp(gl*t)*mq - \
+            Cxl*Exp(gq*tau)*mq + Cxl*Exp(gl*t + gq*tau)*mq - bx*Exp(gl*t)*gl*mq - Cxx*Exp(gl*t)*gl*mq + bx*Exp(gl*t + gq*tau)*gl*mq + \
+            Cxx*Exp(gl*t + gq*tau)*gl*mq - ml*mq + Exp(gl*t)*ml*mq + Exp(gq*tau)*ml*mq - Exp(gl*t + gq*tau)*ml*mq + bq*Exp(gl*t)*gl*ml*t + \
+            Cxq*Exp(gl*t)*gl*ml*t - Exp(gl*t)*gl*ml*mq*t + Exp(gl*t + gq*tau)*gl*ml*mq*t - bq*Cll*tau - Cll*Cxq*tau + \
+            bq*Cll*Exp(gl*t)*tau + Cll*Cxq*Exp(gl*t)*tau + bq*Cxl*Exp(gl*t)*gl*tau + Cxl*Cxq*Exp(gl*t)*gl*tau + Cll*mq*tau - \
+            Cll*Exp(gl*t)*mq*tau - Cll*Exp(gq*tau)*mq*tau + Cll*Exp(gl*t + gq*tau)*mq*tau - Cxl*Exp(gl*t)*gl*mq*tau + \
+            Cxl*Exp(gl*t + gq*tau)*gl*mq*tau + bl*(-1 + Exp(gl*t))*(bq + Cxq - mq + Exp(gq*tau)*mq + Clq*tau) + \
+            Clq*(-1 - Cxl*tau + ml*tau - Cll*tau**2 + Exp(gl*t)*\
+                (1 + bx*gl*tau + Cxx*gl*tau - ml*tau + gl*ml*t*tau + Cll*tau**2 + Cxl*tau*(1 + gl*tau)))),0,t)[0])/gl-nm[1,0]*nm[0,0]
+def cov_xl(t,bx,bg,bl,bq,Cxx,Cxg,Cxl,Cxq,Cgg,Cgl,Cgq,Cll,Clq,Cqq,ml,gl,sl2,mq,gq,sq2,b):
+    cxl = sl2/(2*gl**2)*(1-Exp(-gl*t))**2
+    return cxl+Cll*Exp(-gl*t)*(1-Exp(-gl*t))/gl+Cxl*Exp(-gl*t)
+def cov_xq(t,bx,bg,bl,bq,Cxx,Cxg,Cxl,Cxq,Cgg,Cgl,Cgq,Cll,Clq,Cqq,ml,gl,sl2,mq,gq,sq2,b):
+    return Clq*(1-Exp(-gl*t))*Exp(-gq*t)/gl+Cxq*Exp(-gq*t)
+def cov_gg(t,bx,bg,bl,bq,Cxx,Cxg,Cxl,Cxq,Cgg,Cgl,Cgq,Cll,Clq,Cqq,ml,gl,sl2,mq,gq,sq2,b,nm):
+    def covqq(t1,t2):
+        return sq2/(2*gq)*(np.exp(-np.abs(t1-t2)*gq)-np.exp(-gq*(t1+t2)))
+    def vargt_z0(t1,t2):
+       return covqq(t1,t2)*Exp(2*bx+2*Cxx-2*b*t+(b+bl+2*Cxl)*(t1+t2)+(Cll*(t1+t2)**2)/2.)
+    def covqq_1(r,s):
+        return sq2/(2*gq)*(np.exp(-np.abs(s)*gq)-np.exp(-gq*(r)))
+    def vargt_z0_1(r,s):
+        return covqq_1(r,s)*Exp(2*bx+2*Cxx-2*b*t+(b+bl+2*Cxl)*(r)+(Cll*(r)**2)/2.)
+    def mean_gtz0_part1(tau1):
+        return  Exp(bx + (Cxx + tau1*(2*bl + 2*Cxl + Cll*tau1))/2.)*\
+        (Exp(b*(-2*t + tau1))*mq*(bg + Cxg + Cgl*tau1) - Exp(-2*b*t + b*tau1 - gq*tau1)*mq*(bg + Cxg + Cgl*tau1) + \
+          Exp(-2*b*t + b*tau1 - gq*tau1)*(Cgq + bg*(bq + Cxq + Clq*tau1) + (Cxg + Cgl*tau1)*(bq + Cxq + Clq*tau1)))
+    def mean_gtz0_part2(tau1,tau2):
+        return Exp(2*bx+2*Cxx-gq*(tau1+tau2)+b*(-2*t + tau1 + tau2) + ((tau1 + tau2)*(2*bl + 4*Cxl + Cll*(tau1 + tau2)))/2.)*\
+        (bq**2 + Cqq + (2*Cxq + (-1 + Exp(gq*tau1))*mq + Clq*(tau1 + tau2))*(2*Cxq + (-1 + Exp(gq*tau2))*mq + Clq*(tau1 + tau2)) + \
+          bq*(4*Cxq + (-2 + Exp(gq*tau1) + Exp(gq*tau2))*mq + 2*Clq*(tau1 + tau2)))
+    star = datetime.now()
+    vgtz0 = dblquad(vargt_z0, 0, t, lambda x: 0, lambda x: t)[0]
+    print('part1',datetime.now()-star)
+    print('int',vgtz0)
+##############
+    star = datetime.now()
+    vgtz0 = dblquad(vargt_z0_1, 0, 2*t, lambda x: -t, lambda x: t)[0]
+    print('part1',datetime.now()-star)
+    print('int',vgtz0)
+
+########
+    def vgtz0_(r):
+        return Exp(2*bx+2*Cxx-2*b*t+(b+bl+2*Cxl)*(r)+(Cll*(r)**2)/2.)
+    star = datetime.now()
+    vgtz__=2*(1-Exp(-t))*sl2/(2*gq)*quad( vgtz0_,0,2*t)[0]
+    print('part1 redone',datetime.now()-star)
+    print('int redone',vgtz__)
+#######
+    star = datetime.now()
+    m2gtz0 = (Cgg+bg**2)/Exp(2*b*t) +2*quad(mean_gtz0_part1, 0, t)[0] +\
+              dblquad(mean_gtz0_part2, 0, t, lambda x: 0, lambda x: t)[0]
+    print('part2',datetime.now()-star)
+    return vgtz0 +m2gtz0- nm[1,0]**2
+def cov_gl(t,bx,bg,bl,bq,Cxx,Cxg,Cxl,Cxq,Cgg,Cgl,Cgq,Cll,Clq,Cqq,ml,gl,sl2,mq,gq,sq2,b,nm):
+    return  (bg*bl + Cgl)/Exp((b + gl)*t) + (bg*ml)/Exp(b*t) - (bg*ml)/Exp((b + gl)*t) +\
+            quad(lambda tau: Exp(bx + Cxx/2. - b*t - gl*t + (b + bl + Cxl - gq)*tau + (Cll*tau**2)/2.)*\
+            ((bq + Cxq + (-1 + Exp(gq*tau))*mq)*(Cxl + (-1 + Exp(gl*t))*ml + Cll*tau) + bl*(bq + Cxq - mq + Exp(gq*tau)*mq + Clq*tau) +\
+            Clq*(1 + Cxl*tau + (-1 + Exp(gl*t))*ml*tau + Cll*tau**2)),0,t)[0]-nm[1,0]*nm[2,0]
+def cov_gq(t,bx,bg,bl,bq,Cxx,Cxg,Cxl,Cxq,Cgg,Cgl,Cgq,Cll,Clq,Cqq,ml,gl,sl2,mq,gq,sq2,b,nm):
+    return  quad(lambda tau:(Exp(bx - b*t + b*tau + bl*tau + (Cxx + tau*(2*Cxl + Cll*tau))/2.)*(Exp(gq*(-t + tau)) - Exp(-(gq*(t + tau))))*sq2)/(2.*gq),0,t)[0]+\
+            (bg*bq + Cgq)/Exp((b + gq)*t) + (bg*mq)/Exp(b*t) - (bg*mq)/Exp((b + gq)*t) + quad(lambda tau:\
+            Exp(bx + Cxx/2. - b*t - gq*t + b*tau + bl*tau + Cxl*tau - gq*tau + (Cll*tau**2)/2.)*\
+            (bq**2 + Cqq + (Cxq + (-1 + Exp(gq*t))*mq + Clq*tau)*(Cxq + (-1 + Exp(gq*tau))*mq + Clq*tau) +\
+            bq*(2*Cxq + (-2 + Exp(gq*t) + Exp(gq*tau))*mq + 2*Clq*tau)),0,t)[0]-nm[1,0]*nm[3,0]
+def cov_ll(t,bx,bg,bl,bq,Cxx,Cxg,Cxl,Cxq,Cgg,Cgl,Cgq,Cll,Clq,Cqq,ml,gl,sl2,mq,gq,sq2,b):
+    vl = sl2/(2*gl)*(1-Exp(-gl*t)**2)
+    return Cll*Exp(-gl*t)**2 + vl
+def cov_lq(t,bx,bg,bl,bq,Cxx,Cxg,Cxl,Cxq,Cgg,Cgl,Cgq,Cll,Clq,Cqq,ml,gl,sl2,mq,gq,sq2,b):
+    return  Clq*Exp(-gl*t)*Exp(-gq*t)
+def cov_qq(t,bx,bg,bl,bq,Cxx,Cxg,Cxl,Cxq,Cgg,Cgl,Cgq,Cll,Clq,Cqq,ml,gl,sl2,mq,gq,sq2,b):
+    vq = sq2/(2*gq)*(1-Exp(-gq*t)**2)
+    return vq + Cqq*Exp(-gq*t)**2
+def mean_cov_model(m,C,t,ml,gl,sl2,mq,gq,sq2,b):
+    """Given p(z0)=n(m,C) find p(z1) with no cell division"""
+    nC = np.zeros((4,4))
+    nm = np.zeros((4,1))
+    bx=m[0,0]; bg=m[1,0]; bl=m[2,0]; bq=m[3,0]
+    Cxx=C[0,0];Cxg=C[0,1];Cxl=C[0,2];Cxq=C[0,3];Cgg=C[1,1];Cgl=C[1,2];Cgq=C[1,3];Cll=C[2,2];Clq=C[2,3];Cqq=C[3,3]
+    #####################
+    ######   Mean     ###
+    #####################
+    start = datetime.now()
+    nm[0,0] = mean_x(t,bx,bg,bl,bq,Cxx,Cxg,Cxl,Cxq,Cgg,Cgl,Cgq,Cll,Clq,Cqq,ml,gl,sl2,mq,gq,sq2,b)
+    nm[1,0] = mean_g(t,bx,bg,bl,bq,Cxx,Cxg,Cxl,Cxq,Cgg,Cgl,Cgq,Cll,Clq,Cqq,ml,gl,sl2,mq,gq,sq2,b)
+    nm[2,0] = mean_l(t,bx,bg,bl,bq,Cxx,Cxg,Cxl,Cxq,Cgg,Cgl,Cgq,Cll,Clq,Cqq,ml,gl,sl2,mq,gq,sq2,b)
+    nm[3,0] = mean_q(t,bx,bg,bl,bq,Cxx,Cxg,Cxl,Cxq,Cgg,Cgl,Cgq,Cll,Clq,Cqq,ml,gl,sl2,mq,gq,sq2,b)
+    print("mean",datetime.now()-start)
+    ####################
+    ####### Cov ########
+    ####################
+    start = datetime.now()
+    nC[0,0] = cov_xx(t,bx,bg,bl,bq,Cxx,Cxg,Cxl,Cxq,Cgg,Cgl,Cgq,Cll,Clq,Cqq,ml,gl,sl2,mq,gq,sq2,b)
+    nC[0,1] = cov_xg(t,bx,bg,bl,bq,Cxx,Cxg,Cxl,Cxq,Cgg,Cgl,Cgq,Cll,Clq,Cqq,ml,gl,sl2,mq,gq,sq2,b,nm)
+    nC[0,2] = cov_xl(t,bx,bg,bl,bq,Cxx,Cxg,Cxl,Cxq,Cgg,Cgl,Cgq,Cll,Clq,Cqq,ml,gl,sl2,mq,gq,sq2,b)
+    nC[0,3] = cov_xq(t,bx,bg,bl,bq,Cxx,Cxg,Cxl,Cxq,Cgg,Cgl,Cgq,Cll,Clq,Cqq,ml,gl,sl2,mq,gq,sq2,b)
+    nC[1,2] = cov_gl(t,bx,bg,bl,bq,Cxx,Cxg,Cxl,Cxq,Cgg,Cgl,Cgq,Cll,Clq,Cqq,ml,gl,sl2,mq,gq,sq2,b,nm)
+    nC[1,3] = cov_gq(t,bx,bg,bl,bq,Cxx,Cxg,Cxl,Cxq,Cgg,Cgl,Cgq,Cll,Clq,Cqq,ml,gl,sl2,mq,gq,sq2,b,nm)
+    nC[2,2] = cov_ll(t,bx,bg,bl,bq,Cxx,Cxg,Cxl,Cxq,Cgg,Cgl,Cgq,Cll,Clq,Cqq,ml,gl,sl2,mq,gq,sq2,b)
+    nC[2,3] = cov_lq(t,bx,bg,bl,bq,Cxx,Cxg,Cxl,Cxq,Cgg,Cgl,Cgq,Cll,Clq,Cqq,ml,gl,sl2,mq,gq,sq2,b)
+    nC[3,3] = cov_qq(t,bx,bg,bl,bq,Cxx,Cxg,Cxl,Cxq,Cgg,Cgl,Cgq,Cll,Clq,Cqq,ml,gl,sl2,mq,gq,sq2,b)
+    print('cov',datetime.now()-start)
+    start = datetime.now()
+    nC[1,1] = cov_gg(t,bx,bg,bl,bq,Cxx,Cxg,Cxl,Cxq,Cgg,Cgl,Cgq,Cll,Clq,Cqq,ml,gl,sl2,mq,gq,sq2,b,nm)
+    print('var',datetime.now()-start)
+    ####################
+    #### return     ####
+    ####################
+    nC[1,0]=nC[0,1];nC[2,0]=nC[0,2];nC[2,1]=nC[1,2];nC[3,0]=nC[0,3];nC[3,1]=nC[1,3];nC[3,2]=nC[2,3]
+    return nm,nC
